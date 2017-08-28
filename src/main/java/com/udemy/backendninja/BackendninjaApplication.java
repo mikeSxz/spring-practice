ff360bbd3f87ae3a98e6c41d0187e01f424c74f3
😎Miguel Angel Suarez Montiel <masuarez@ohkasystems.com>
2017-08-28 16:21:39 -0500

    Ejemplo de spring batch

--- src/main/java/com/udemy/backendninja/BackendninjaApplication.java
+++ src/main/java/com/udemy/backendninja/BackendninjaApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("com.udemy.backendninja")
 @EnableJpaRepositories("com.udemy.backendninja.repository")
 @EntityScan("com.udemy.backendninja.entity")
+@EnableScheduling
 public class BackendninjaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendninjaApplication.class, args);
